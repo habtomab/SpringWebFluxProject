989eeb1bb518737894b106145bff878885131913
habtomgebre <hatomg127@gmail.com>
2025-01-10 15:00:17 -0600

    new spring reactive project created

---
+++ springwebFlux_Microservice/src/main/java/com/springwebFlux_Microservice/SpringwebFluxMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springwebFlux_Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebFluxMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebFluxMicroserviceApplication.class, args);
+	}
+
+}
---
+++ springwebFlux_Microservice/src/test/java/com/springwebFlux_Microservice/SpringwebFluxMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebFlux_Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebFluxMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
